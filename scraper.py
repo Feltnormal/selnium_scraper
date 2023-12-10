cda8e9a2837b21b45242aab63a3a9d60cb79d16c
gerard_huang <u6080257@anu.edu.au>
2023-12-10 17:35:58 +1100

    updated gitignore and using sample track as test

--- scraper.py
+++ scraper.py
@@ -3,17 +3,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-import time
-
 username = ""
 password = ""
 
-driver = webdriver.Firefox()
-driver.get('https://accounts.spotify.com/en/login?continue=https%3A%2F%2Fopen.spotify.com%2F')
+options = webdriver.FirefoxOptions()
+driver = webdriver.Firefox()  # expecting this to be in local repo
+driver.get('https://open.spotify.com/track/0WSEq9Ko4kFPt8yo3ICd6T?si=e2c993506d17435b&nd=1')
 driver.implicitly_wait(5)
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".LKFFk88SIRC9QKKUWR5u > button:nth-child(2)"))).click()
 
 driver.find_element("id", "login-username").send_keys(username)
 driver.find_element("id", "login-password").send_keys(password)
 driver.find_element("id", "login-button").click()
 
 driver.implicitly_wait(5)
+
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='onetrust-close-btn-container']//button[@aria-label='Close']"))).click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@data-testid='action-bar-row']//button[@data-testid='play-button' and @aria-label='Play']"))).click()
