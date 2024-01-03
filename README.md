65870a654fccf93be5a632a14d109491ae9083f3
gerard_huang <u6080257@anu.edu.au>
2024-01-03 14:43:23 +1100

    Updated repo with FF profile config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # selnium_scraper
+
+Please unzip the firefox profile and setup account creds in _secrets.py for live testing
--- scraper.py
+++ scraper.py
@@ -2,14 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.options import Options, FirefoxProfile
 from _secrets import username, password
-
 options = Options()
+options.profile = FirefoxProfile("vtb1cawa.sel_user")
 options.set_preference("media.eme.enabled", True)
 options.set_preference("media.gmp-manager.updateEnabled", True)
+
 driver = webdriver.Firefox(options=options)  # expecting this to be in local repo
-driver.get('https://open.spotify.com/track/0WSEq9Ko4kFPt8yo3ICd6T?si=e2c993506d17435b&nd=1')
+driver.get("https://open.spotify.com/album/39hEC3Wb2aJCSpHYGjJ15Y?si=GKACjgF0RLSPij29rcMW8A")
 driver.implicitly_wait(5)
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".LKFFk88SIRC9QKKUWR5u > button:nth-child(2)"))).click()
 
